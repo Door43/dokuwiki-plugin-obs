bec96fed55e7e6af007af6a1645ff3e50a080758
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2015-01-14 11:37:20 -0500

    Do not include jquery.min

--- script.js
+++
@@ -1,4 +0,0 @@
-// DO NOT REMOVE THIS FILE!!
-// Even though the lines appear to be commented out, dokuwiki uses them to load the javascript libraries we need
-/* DOKUWIKI:include_once /lib/scripts/jquery/jquery.min.js */
-/* DOKUWIKI:include_once /lib/scripts/jquery/jquery-ui.min.js */
